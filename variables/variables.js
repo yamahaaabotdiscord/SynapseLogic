63e837a8b759c47dc52be233cf9ab359ab9d0e6e
yamahaaabotdiscord <150445372+yamahaaabotdiscord@users.noreply.github.com>
2023-11-10 13:58:14 +0100

    Variables
    Les variables

---
+++ variables/variables.js
@@ -0,0 +1,15 @@
+module.exports = client => {
+    client.variables({
+    
+     afk: "false",
+    afkmsg: "Aucune raison",
+    time: "",
+    maintenance: "false",
+        
+
+
+
+    });
+  }
+  
+  
